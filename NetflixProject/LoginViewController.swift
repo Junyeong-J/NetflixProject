f6803e9f9b1b3a3f8372572230dadfbd67eaacda
JunYeong-J <eeot2827@gmail.com>
2024-05-21 01:50:02 +0900

    로그인뷰 코드 간결화

--- NetflixProject/LoginViewController.swift
+++ NetflixProject/LoginViewController.swift
@@ -9,11 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var nicknameTextField: UITextField!
-    @IBOutlet var locationTextField: UITextField!
-    @IBOutlet var codeTextField: UITextField!
+    @IBOutlet var textFieldLists: [UITextField]!
+    
     @IBOutlet var joinButton: UIButton!
     @IBOutlet var setSwitch: UISwitch!
     
@@ -21,48 +18,12 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         UITabBar.appearance().tintColor = .white
         
-        emailTextField.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        emailTextField.tintColor = .white
-        emailTextField.textColor = .white
-        emailTextField.keyboardType = .emailAddress
-        emailTextField.textAlignment = .left
-        emailTextField.borderStyle = .bezel
-        emailTextField.backgroundColor = .darkGray
-        
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        passwordTextField.tintColor = .white
-        passwordTextField.textColor = .white
-        passwordTextField.isSecureTextEntry = true
-        passwordTextField.keyboardType = .default
-        passwordTextField.textAlignment = .left
-        passwordTextField.borderStyle = .bezel
-        passwordTextField.backgroundColor = .darkGray
-        
-        nicknameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        nicknameTextField.tintColor = .white
-        nicknameTextField.textColor = .white
-        nicknameTextField.keyboardType = .default
-        nicknameTextField.textAlignment = .left
-        nicknameTextField.borderStyle = .roundedRect
-        nicknameTextField.backgroundColor = .darkGray
-        
-        locationTextField.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        locationTextField.tintColor = .white
-        locationTextField.textColor = .white
-        locationTextField.keyboardType = .default
-        locationTextField.textAlignment = .left
-        locationTextField.borderStyle = .line
-        locationTextField.backgroundColor = .darkGray
-        
-        codeTextField.attributedPlaceholder = NSAttributedString(string: "추천 코드 입력", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        codeTextField.tintColor = .white
-        codeTextField.textColor = .white
-        codeTextField.keyboardType = .numberPad
-        codeTextField.textAlignment = .left
-        codeTextField.borderStyle = .none
-        codeTextField.backgroundColor = .darkGray
-        
-        
+        textFieldUI(textField: textFieldLists[0], placeholderText: "이메일 주소 또는 전화번호", keyBoardType: .emailAddress)
+        textFieldUI(textField: textFieldLists[1], placeholderText: "비밀번호", keyBoardType: .default)
+        textFieldUI(textField: textFieldLists[2], placeholderText: "닉네임", keyBoardType: .default)
+        textFieldUI(textField: textFieldLists[3], placeholderText: "위치", keyBoardType: .default)
+        textFieldUI(textField: textFieldLists[4], placeholderText: "추천 코드 입력", keyBoardType: .numberPad)
+
         joinButton.setTitle("회원가입", for: .normal)
         joinButton.setTitleColor(.black, for: .normal)
         joinButton.backgroundColor = .white
@@ -70,6 +31,18 @@ class LoginViewController: UIViewController {
         setSwitch.setOn(true, animated: true)
         setSwitch.onTintColor = .red
         setSwitch.thumbTintColor = .gray
+    }
+    
+    func textFieldUI(textField: UITextField, placeholderText: String, keyBoardType: UIKeyboardType) {
+        
+        textField.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        textField.tintColor = .white
+        textField.textColor = .white
+        textField.keyboardType = keyBoardType
+        textField.textAlignment = .left
+        textField.borderStyle = .none
+        textField.backgroundColor = .darkGray
+        
     }
     
     @IBAction func emailTextFieldReturnTapped(_ sender: UITextField) {
@@ -81,13 +54,7 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func joinButtonTapped(_ sender: UIButton) {
-        
-        emailTextField.resignFirstResponder()
-        passwordTextField.resignFirstResponder()
-        nicknameTextField.resignFirstResponder()
-        locationTextField.resignFirstResponder()
-        codeTextField.resignFirstResponder()
-        
+        view.endEditing(true)
     }
     
 }
